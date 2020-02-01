1efa10122a13d352fb18eb447f13d34df76edefe
doyeon1725 <doyeon1725@gmail.com>
2020-02-02 01:25:59 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express();
 
-app.get('api/hello', function(req, res){
+app.get('api/hello', function(req, res) {
     res.send('hello world');
 });
 
-app.listen(8000, () => {
+app.listen(8000, function() {
    console.log('server is listening 8000');
 });
